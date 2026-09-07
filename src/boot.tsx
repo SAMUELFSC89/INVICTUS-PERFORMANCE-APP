dc5efabb7404c9940f38733e4cc7fabeeeb0d528
SAMUELFSC89 <samuelfsc89@gmail.com>
2026-09-06 21:37:57 -0300

    style(cardio): load modern cardio visual layer

--- src/boot.tsx
+++ src/boot.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './styles/readability.css';
+import './styles/ChallengeCardioModern.css';
 import GlobalErrorBoundary from './components/GlobalErrorBoundary';
 // #mapa-ios-2026-09-04: efeito colateral, sem exports usados aqui de proposito.
 //
