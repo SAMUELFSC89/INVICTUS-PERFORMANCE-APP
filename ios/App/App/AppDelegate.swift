7c41129f1dbcaa19a4ed803569f046fe147eed91
SAMUELFSC89 <samuelfsc89@gmail.com>
2026-09-13 17:13:55 -0300

    audit: wire iOS APNs registration into Capacitor

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -11,14 +11,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    // @capacitor/push-notifications não recebe o token APNs diretamente do
+    // UIApplicationDelegate. O contrato oficial do plugin exige encaminhar o
+    // sucesso/erro por NotificationCenter para que o evento `registration`
+    // (e `registrationError`) chegue ao JavaScript. Sem estes callbacks o app
+    // podia ter a capability `aps-environment` correta e ainda assim nunca
+    // persistir o token iOS no backend.
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        NotificationCenter.default.post(name: .capacitorDidRegisterForRemoteNotifications, object: deviceToken)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        NotificationCenter.default.post(name: .capacitorDidFailToRegisterForRemoteNotifications, object: error)
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -26,11 +38,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        // Restart any tasks that were paused (or not yet started) while the application was inactive.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate. Save data if appropriate.
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
