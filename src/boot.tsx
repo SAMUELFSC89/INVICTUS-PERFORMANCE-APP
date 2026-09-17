5d009a89394f62a4e55593bd48c604162e9213ba
SAMUELFSC89 <samuelfsc89@gmail.com>
2026-09-16 22:07:49 -0300

    Load fixed dark surface contrast overrides

--- src/boot.tsx
+++ src/boot.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './styles/readability.css';
+import './styles/fixedDarkSurfaces.css';
 import './styles/ChallengeCardioModern.css';
 import './styles/ChallengeCardioIndoor.css';
 import './styles/ChallengeCardioBackgroundFix.css';
